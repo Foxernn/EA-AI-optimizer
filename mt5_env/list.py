f46d13280c645ff19f272fa824b429fbd2d22648
Foxernn <charliehjfox@gmail.com>
2025-03-01 00:47:05 +0100

    Add files via upload

---
+++ mt5_env/list.py
@@ -0,0 +1,11 @@
+Location,Full Name,Email Address,Company Name,Website Link,Phone Number
+"Sitapura Industrial Area, Jaipur","Rohit","rohit2may@gmail.com","Banking Suvidha Centre","https://mail.bankingsuvidhacentre.com","9694365958"
+"Berlin","Mohrenstrasse 37","walmaonlinebankingservice@gmail.com","Walma Online Banking","https://walma-online-banking.yolasite.com","+1 234 567 8910"
+"Buffalo, WY","Clear Creek Wood Bank","clearcreekwoodbank@gmail.com","Clear Creek Wood Bank","https://www.clearcreekwoodbank.com","307-620-0631"
+"Valdez, AK","Valdez Food Bank","emergencyfoodvaldez@gmail.com","Valdez Food Bank","https://www.valdezfoodbank.org","907-835-3663"
+"Brooklyn, NY","Bank Tank Charters","banktankcharters@gmail.com","Bank Tank Charters","https://banktankcharters.com","646-335-7157"
+"Toronto, Canada","West Lodge Food Bank Tenants Association","103.105.westlodge.tenants@gmail.com","West Lodge Food Bank","https://westlodgefoodbank.com",N/A
+"Minneapolis, MN","The Food Bank Eatery Team","thefoodbanktruck@gmail.com","The Food Bank Eatery","https://www.thefoodbankeatery.com",N/A
+"San Bernardino, CA","Mojave River Mitigation Bank Contact Person","mojavedetobank@gmail.com","Mojave River Watershed Mitigation Bank",N/A,"(310) 795-5616"
+"Ogdensburg, NY","Ogdensburg Land Bank Corporation Team","ogdensburglandbank@gmail.com","Ogdensburg Land Bank Corporation","https://asmith4227.wixsite.com/ogdensburglandbank/ogdensburglandbank/ogdensburglandbank",3153937150
+
